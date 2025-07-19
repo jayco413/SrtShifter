2566a0d7c605a90e0a0ed66e409857a6535a4711
jayco413 <jcovey323@gmail.com>
2025-07-18 22:34:06 -0400

    Add comprehensive unit tests and clean up

--- SrtShifterUnitTests/UnitTests.cs
+++ SrtShifterUnitTests/UnitTests.cs
@@ -1,27 +1,62 @@
-using Microsoft.UI.Xaml.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+using SrtShifter.Models;
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace SrtShifterUnitTests
 {
     [TestClass]
-    public partial class UnitTest1
+    public class UnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static string GetSamplePath(string fileName)
         {
-            Assert.AreEqual(0, 0);
+            var dir = Path.GetDirectoryName(typeof(UnitTests).Assembly.Location) ?? string.Empty;
+            return Path.Combine(dir, fileName);
         }
 
-        // Use the UITestMethod attribute for tests that need to run on the UI thread.
-        [UITestMethod]
-        public void TestMethod2()
+        [TestMethod]
+        public void MovParser_ReturnsExpectedDuration()
         {
-            var grid = new Grid();
-            Assert.AreEqual(0, grid.MinWidth);
+            var movPath = GetSamplePath("sample.mov");
+            var duration = SrtShifter.MovParser.GetDuration(movPath);
+            var expected = TimeSpan.FromSeconds(3.7120416);
+            Assert.IsTrue(Math.Abs((duration - expected).TotalMilliseconds) <= 1, $"Duration {duration} not within tolerance");
         }
+
+        [TestMethod]
+        public void SrtFile_LoadsEntries()
+        {
+            var srtPath = GetSamplePath("sample.srt");
+            var srt = SrtFile.Load(srtPath);
+            Assert.IsTrue(srt.Entries.Count > 0, "No entries parsed");
+            var first = srt.Entries.First();
+            Assert.AreEqual(1, first.Index);
+            Assert.AreEqual(TimeSpan.Parse("00:00:00.367"), first.Start);
+            Assert.AreEqual(TimeSpan.Parse("00:00:03.534"), first.End);
+            Assert.IsTrue(first.Lines.Count > 0);
+        }
+
+        [TestMethod]
+        public void SrtFile_ShiftAndSave_RoundTrips()
+        {
+            var srtPath = GetSamplePath("sample.srt");
+            var temp = Path.GetTempFileName();
+            try
+            {
+                var srt = SrtFile.Load(srtPath);
+                var offset = TimeSpan.FromSeconds(5);
+                srt.Shift(offset);
+                srt.Save(temp);
+
+                var shifted = SrtFile.Load(temp);
+                Assert.AreEqual(srt.Entries.First().Start, shifted.Entries.First().Start);
+            }
+            finally
+            {
+                File.Delete(temp);
+            }
+        }
+
     }
 }
